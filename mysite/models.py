af278b4365b21d201fe2df26440302cd672268dd
Tom Victor <vjtomvictor@icloud.com>
2016-11-09 16:20:37 +0530

    More changes

--- mysite/models.py
+++ mysite/models.py
@@ -10,3 +10,13 @@ class Mqtt(models.Model):
 
     def __str__(self):
         return self.msg
+
+class Gps(models.Model):
+    lat = models.CharField(max_length=500,blank=True)
+    long = models.CharField(max_length=500,blank=True)
+    speed = models.CharField(max_length=500,blank=True)
+    time = models.DateTimeField(auto_now=False,auto_now_add=True)
+    deviceId = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.deviceId
